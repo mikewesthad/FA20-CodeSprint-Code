7ea11998852f876ed9e2c8cec5092e1d7e7f4299
Michael Hadley <michaelwesthadley@gmail.com>
2020-11-05 12:51:28 -0600

    Add data type starter for week 05

---
+++ Code Sprint B/Week 05/data-types-demo-starter/src/components/app.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  const onSampleAdd = async () => {};
+
+  const printPeopleData = async () => {};
+
+  return (
+    <div>
+      <button onClick={onSampleAdd}>Add Grace Hopper</button>
+      <br />
+      <button onClick={printPeopleData}>Print Data</button>
+    </div>
+  );
+}
+
+export default App;
